c0f928c03e6b2ca6447598e9280520a0f577ffc3
Adam Klíma <adam.klima@infis.cz>
2023-10-31 19:40:10 +0100

    .

--- src/CyclesTest/cv5.java
+++ src/CyclesTest/cv5.java
@@ -14,6 +14,29 @@ public class cv5 {
             }
             System.out.println();
             num2 = num2 + num;
+
+            // 1.Čtěte vstupy (celá čísla) od uživatele. Načítání vstupů uživatel ukončí zadáním čísla -1.
+            // Jakmile cyklus doběhne, vypište součet všech zadaných čísel. (Můžete předpokládat, že uživatel
+            // bude zadávat pouze a jedině čísla)
+            //
+            // 2.Pomocí cyklů vypočtěte faktoriál čísla. a.Použijte stejný
+            // mechanismus na vypočtení n-té mocniny čísla (Ano, jde to pomocí Math.pow(), ta ale je neefekƟvní
+            // a zároveň se jedná o jednoduchou metodu)
+            //
+            // 3.Napište Ɵpovací hru: a.Vygenerujte náhodné
+            // číslo od 1 do 100. b.Od uživatele budete číst Ɵpy (celá čísla). c.Dle Ɵpu od uživatele vypíšete,
+            // jestli zadal číslo moc malé nebo číslo moc velké. d.Pokud uživatel Ɵpl správné číslo, ukončete program.
+            //
+            // 4.Vygenerujte 100000x náhodné číslo od 1 do 100. Vypište, kolikrát padlo číslo 42.
+            //
+            // 5.Vypište matici (‚tabulku‘) nxn celých čísel od 1 do n2.
+            // Př: n = 3, vypíšu 3 x 3 hodnoty od 1 do 9, tj.:
+            //
+            // 1    2    3
+            // 4    5    6
+            // 7    8    9
+            //
+            // možná se bude hodit, co je Math.sqrt()...
         }
     }
 }
