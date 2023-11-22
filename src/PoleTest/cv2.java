caa220e4fa2ad855049a886c8f452cec292e1d0e
Adam <adam.klima@infis.cz>
2023-11-22 18:37:16 +0100

    .

---
+++ src/PoleTest/cv2.java
@@ -0,0 +1,30 @@
+package PoleTest;
+
+import java.util.Arrays;
+public class cv2 {
+
+    public static void main(String[] args) {
+        int[] pole = new int[5];
+        int sudý = 0;
+        int lichý = 0;
+
+        for (int i = 0; i < pole.length; i++) {
+            pole[i] = (int) (Math.random() * 100 + 1);
+            if (pole[i] % 2 == 0) {
+                sudý += pole[i];
+            } else {
+                lichý += pole[i];
+            }
+        }
+
+        System.out.println(Arrays.toString(pole));
+        System.out.println("sudý: "+sudý);
+        System.out.println("lichý: "+lichý);
+
+
+    }
+}
+
+
+
+
---
+++ src/PoleTest/cv5.java
@@ -0,0 +1,23 @@
+package PoleTest;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class cv5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] pole = new int[10];
+        int num;
+        for (int i = 0; i < pole.length; i++) {
+            System.out.println("Zadej číslo od 10 do 100: ");
+            num = sc.nextInt();
+            while (num < 10 || num > 100) {
+                System.out.println("Číslo neakceptováno. Zadej číslo od 10 do 100: ");
+                num = sc.nextInt();
+            }
+            pole[i] = num;
+        }
+        System.out.println(Arrays.toString(pole));
+    }
+}
